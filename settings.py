01334cdfa86f919b00d75a6d9f0e4eb97f29a1ad
sihannb <hasimbola1706@gmail.com>
2026-03-31 10:37:05 +0100

    Add Procfile

--- settings.py
+++ settings.py
@@ -3,10 +3,17 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
 class Config:
     DEBUG = False
     DATABASE_URL = os.getenv("DATABASE_URL")
 
+    SQLALCHEMY_DATABASE_URI = DATABASE_URL
+    SQLALCHEMY_ENGINE_OPTIONS = {
+        "connect_args": {"connect_timeout": 15},
+        "pool_pre_ping": True,
+        "pool_recycle": 300,
+    }
 class DevelopmentConfig(Config):
     DEBUG = True
 
