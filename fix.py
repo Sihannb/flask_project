7e47755ebebb56755e5f544077ce489551e8e825
sihannb <hasimbola1706@gmail.com>
2026-04-02 16:55:53 +0100

    Commit final

--- fix.py
+++
@@ -1,9 +0,0 @@
-with open("requirements.txt", "rb") as f:
-    content = f.read()
-
-# enlever BOM
-content = content.lstrip(b'\xef\xbb\xbf')
-
-with open("requirements.txt", "wb") as f:
-    f.write(content)
-print("Fichier nettoyé ✅")
